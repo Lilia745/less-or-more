a9d4da58ad5383a31e05f7288c9f4f78e2333a55
Lilia Hasratyan <hasratyanlilia@gmail.com>
2024-08-03 00:09:57 +0400

    finish light dark button

--- src/Components/Box/Box.jsx
+++ src/Components/Box/Box.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import IonIcon from "../IonIcon/IonIcon";
 
 function Box() {
     const [showText,setShowText] = useState(true)
@@ -11,12 +10,23 @@ function Box() {
     const clickFunction = ()=>{
         setShowText(!showText)
     }
+    const [isDark, setIsDark] = useState(true)
+    const func = ()=>{
+        setIsDark(!isDark)
+    }
     return(
-        <div className="box">
-            <IonIcon/>
-            <div className="root">
-                <h2>Information</h2>
-                <p onClick={clickFunction}>{text} <span className="span">{showText ? "Less" : "More"}</span> </p>
+        <div className="box" style={{background : isDark ? "White" : "Black"}}>
+            <div className="ionIconsDiv">
+                <div className="sunDiv" onClick={func}>
+                    {isDark ? <ion-icon name="sunny-outline"></ion-icon> : 
+                    <div className="moonDiv">
+                        <ion-icon name="moon-outline"></ion-icon>
+                    </div>}
+                </div>
+            </div>
+            <div className="root" style={{border : isDark ? "1px solid black" : "1px solid white"}}>
+                <h2 style={{color: isDark ? "black" : "white"}}>Information</h2>
+                <p onClick={clickFunction} style={{color: isDark ? "black" : "white"}}>{text} <span className="span">{showText ? "Less" : "More"}</span> </p>
             </div>
         </div>
     );
--- src/Components/IonIcon/IonIcon.jsx
+++
@@ -1,8 +0,0 @@
-function IonIcon() {
-    return(
-        <div className="IonIconsDiv">
-            <ion-icon name="sunny-outline"></ion-icon>
-        </div>
-    );
-}
-export default IonIcon
