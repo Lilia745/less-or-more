44c128dd070de40849bc084d50a2db88d7818e8a
Lilia Hasratyan <hasratyanlilia@gmail.com>
2024-08-03 02:14:02 +0400

    finish task

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Box from './Components/Box/Box';
 function App() {
   return (
     <div className="App">
-      <Box/>
+      <Box text= "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi vel asperiores quos culpa quasi placeat molestias.Animi sit commodi illo rem aliquam!Culpa reiciendis saepe modi,pariatur enim fugit laborum?" />
     </div>
   );
 }
--- src/Components/Box/Box.jsx
+++ src/Components/Box/Box.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
 
-function Box() {
+function Box(props) {
     const [showText,setShowText] = useState(true)
-    const text = `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Excepturi vel asperiores quos culpa quasi placeat molestias.
-                  Animi sit commodi illo rem aliquam!
-                  Culpa reiciendis saepe modi,
-                  pariatur enim fugit laborum?`
-    const clickFunction = ()=>{
-        setShowText(!showText)
-    }
     const [isDark, setIsDark] = useState(true)
     const func = ()=>{
         setIsDark(!isDark)
     }
+    const clickFunc = (e)=>{
+        e.preventDefault()
+        setShowText(!showText)
+    }
+    // const text = props.text
+    // const max = 50
+    // if (text.length<=max) {
+    //     return(
+    //         <span>{text}</span>
+    //     )
+    // }
+    const {text, max=55} = props
+
     return(
         <div className="box" style={{background : isDark ? "White" : "Black"}}>
             <div className="ionIconsDiv">
@@ -26,9 +31,10 @@ function Box() {
             </div>
             <div className="root" style={{border : isDark ? "1px solid black" : "1px solid white"}}>
                 <h2 style={{color: isDark ? "black" : "white"}}>Information</h2>
-                <p onClick={clickFunction} style={{color: isDark ? "black" : "white"}}>{text} <span className="span">{showText ? "Less" : "More"}</span> </p>
+                <span style={{color : isDark ? "black" : "white"}}>{showText ? `${text.substring(0,max)}...`: text}
+                <a href="" onClick={clickFunc} className="span">{showText ? "More" : "Less"}</a></span>
             </div>
         </div>
-    );
+    ); 
 }
 export default Box
