966942361aae412c991d048c5c7756cc7793517d
Lilia Hasratyan <hasratyanlilia@gmail.com>
2024-08-02 23:31:18 +0400

    added useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from './Components/Box/Box';
 
 function App() {
   return (
     <div className="App">
-      
+      <Box/>
     </div>
   );
 }
---
+++ src/Components/Box/Box.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function Box() {
+    const [showText,setShowText] = useState(true)
+    const text = `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi vel asperiores quos culpa quasi placeat molestias.
+                  Animi sit commodi illo rem aliquam!
+                  Culpa reiciendis saepe modi,
+                  pariatur enim fugit laborum?`
+    const clickFunction = ()=>{
+        setShowText(!showText)
+    }
+    return(
+        <div className="box">
+            <div className="root">
+                <h2>Information</h2>
+                <p onClick={clickFunction}>{text} <span className="span">{showText ? "Less" : "More"}</span> </p>
+            </div>
+        </div>
+    );
+}
+export default Box
